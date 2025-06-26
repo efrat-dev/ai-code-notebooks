ed470e3c720cf01f6f3d1fd715191b3b296fa9d7
efchbd1013 <efchbd1013@gmail.com>
2025-06-26 12:05:43 +0300

    Add config.js file instead of inline script in index.html

---
+++ docs/config.js
@@ -0,0 +1,4 @@
+const GITHUB_USER = 'efrat-dev';
+const GITHUB_REPO = 'ai-code-notebooks';
+const GITHUB_TREE_URL = `https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPO}/git/trees/main?recursive=1`;
+const NOTEBOOKS_FOLDER = 'notebooks/';
